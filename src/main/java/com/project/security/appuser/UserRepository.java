1935af419f4ae2f2b936339e85b9ca53d3539e91
Bartemnius <wojtekdluski@wp.pl>
2024-01-14 12:44:13 +0100

    Add UserApp Service and Repository

---
+++ src/main/java/com/project/security/appuser/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.security.appuser;
+
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository {
+    Optional<AppUser> findByEmail(String email);
+}
