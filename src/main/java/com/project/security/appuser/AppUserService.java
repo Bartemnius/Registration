77ec21197cef0df6bb739d2a63821623238f76c0
Bartemnius <wojtekdluski@wp.pl>
2024-01-16 16:31:05 +0100

    Create confirmation token.
    Token to confirm user is created when the POST request happens.
    The token will be used in email which will be send to user to confirm that his credentials are valid.

--- src/main/java/com/project/security/appuser/AppUserService.java
+++ src/main/java/com/project/security/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.project.security.appuser;
 
+import com.project.security.registration.token.ConfirmationToken;
+import com.project.security.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -23,7 +29,7 @@ public class AppUserService implements UserDetailsService {
     public String singUpUser(AppUser appUser) {
         boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
                 .isPresent();
-        if(userExists) {
+        if (userExists) {
             throw new IllegalStateException("User already exists!");
         }
 
@@ -32,7 +38,20 @@ public class AppUserService implements UserDetailsService {
 
         appUserRepository.save(appUser);
 
-        //todo send confirmation token
-        return "sigUpUser works";
+        //Create confirmation token and send it
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        //TODO: send email
+
+        return token;
     }
 }
---
+++ src/main/java/com/project/security/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,14 @@
+package com.project.security.registration.token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken confirmationToken) {
+        confirmationTokenRepository.save(confirmationToken);
+    }
+}
