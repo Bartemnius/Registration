537a24c32f8c63a05f28b73ccf3f02d538a5c288
Bartemnius <wojtekdluski@wp.pl>
2024-01-16 12:27:21 +0100

    Security module added.
    Web security made so anyone can log in for now. Password encryption has
    been made.
    Additional changes were made to make sure the basic post request can be send via Postman.

--- src/main/java/com/project/security/registration/RegistrationController.java
+++ src/main/java/com/project/security/registration/RegistrationController.java
@@ -1,5 +1,7 @@
 package com.project.security.registration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/registration")
 public class RegistrationController {
 
+    @Autowired
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
---
+++ src/main/java/com/project/security/security/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.project.security.security.config;
+
+import com.project.security.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain configureSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/api/v*/registration/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .formLogin(Customizer.withDefaults())
+                .authenticationProvider(daoAuthenticationProvider())
+                .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+
+}
