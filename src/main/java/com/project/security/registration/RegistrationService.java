a206eec09ef9b306f833ea6a638c0a453bbb96ac
Bartemnius <wojtekdluski@wp.pl>
2024-01-16 17:20:28 +0100

    Add token confirmation process to enable user.

--- src/main/java/com/project/security/registration/RegistrationService.java
+++ src/main/java/com/project/security/registration/RegistrationService.java
@@ -3,8 +3,13 @@ package com.project.security.registration;
 import com.project.security.appuser.AppUser;
 import com.project.security.appuser.AppUserRole;
 import com.project.security.appuser.AppUserService;
+import com.project.security.registration.token.ConfirmationToken;
+import com.project.security.registration.token.ConfirmationTokenService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +17,7 @@ public class RegistrationService {
 
     private final AppUserService appUserService;
     private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
     
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
@@ -25,5 +31,26 @@ public class RegistrationService {
                 request.getPassword(),
                 AppUserRole.USER
         ));
+    }
+
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token);
+
+        if(confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("Email already confirmed!");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if(expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("Token expired!");
+        }
+
+        confirmationTokenService.enableAppUser(confirmationToken.getAppUser().getEmail());
+
+        return "Confirmed!";
     }
 }
--- src/main/java/com/project/security/registration/token/ConfirmationTokenService.java
+++ src/main/java/com/project/security/registration/token/ConfirmationTokenService.java
@@ -1,14 +1,30 @@
 package com.project.security.registration.token;
 
+import com.project.security.appuser.AppUserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenService {
     private final ConfirmationTokenRepository confirmationTokenRepository;
+    private final AppUserRepository appUserRepository;
 
     public void saveConfirmationToken(ConfirmationToken confirmationToken) {
         confirmationTokenRepository.save(confirmationToken);
+    }
+
+    public ConfirmationToken getToken(String token) {
+        Optional<ConfirmationToken> confirmationToken = confirmationTokenRepository.findByToken(token);
+        if(confirmationToken.isPresent()) {
+            return confirmationToken.get();
+        }
+        else throw new IllegalStateException("Token not found");
+    }
+
+    public void enableAppUser(String email) {
+        appUserRepository.findByEmail(email).get().setEnabled(true);
     }
 }
